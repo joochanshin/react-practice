a4f05565c3d279889f33323312ae5d2784743d06
JooChan <14jshin@gmail.com>
2018-02-15 14:50:29 -0500

    changed greeting

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
+	res.send({ bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000 //User port Heroku is providing or use 5000
